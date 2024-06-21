f07cf4e8614f2a2d0d129996cd6642fb1255e9e1
VishalKumar1111 <aryan11gs@gmail.com>
2024-06-21 19:38:03 +0530

    first commit

---
+++ studdata/studdata/urls.py
@@ -0,0 +1,28 @@
+"""
+URL configuration for studdata project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from enroll.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    path('',UserShow.as_view(),name='addstud'),
+    path('updstud/<int:id>/',UserUpdate.as_view(),name='updstud'),
+    path('delete_data/<int:id>/',UserDelete.as_view(),name='delete_data')
+]
+
